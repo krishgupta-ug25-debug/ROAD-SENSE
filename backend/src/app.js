6b0b895a553278b1f6ff8ea00b23191bd587f39c
anonymous1191 <krish.gupta-ug25@nsut.ac.in>
2026-09-02 22:35:38 +0530

    Refactor app.js to add request logging middleware before detection router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 const express=require("express")
 const app=express()
 const cors = require("cors")
+
 app.use(cors())
 app.use(express.json())
-const detectionrouter=require("./routers/detectionroute")
 
-app.use("/yolo/api",detectionrouter.detectionrouter)
+app.use((req, res, next) => {
+    console.log("REQUEST RECEIVED:", req.method, req.originalUrl);
+    next();
+});
+
+const detectionrouter=require("./routers/detectionroute")
+app.use("/yolo/api",detectionrouter)
 
 module.exports=app
